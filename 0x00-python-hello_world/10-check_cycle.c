1f0fdf85393e130c39dfb849140788ebc3ff4898
salim aliyu musawa <salemaliyu1992@gmail.com>
2023-10-02 16:56:46 -0700

    Linked list cycle

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * check_cycle - ...
+ * @list: ...
+ * Return: ..
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *ptr1, *ptr2;
+
+	if (list == NULL || list->next == NULL)
+		return (0);
+
+	ptr1 = ptr2 = list;
+	while (ptr1 != NULL && ptr2 != NULL)
+	{
+		ptr1 = ptr1->next;
+		ptr2 = ptr2->next->next;
+		if (ptr1 == ptr2)
+			return (1);
+	}
+	return (0);
+}
