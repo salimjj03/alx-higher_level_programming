530cce25ccec5d8c46891080ebdde26e8c591fce
salim aliyu musawa <salemaliyu1992@gmail.com>
2023-10-05 14:34:40 -0700

    variable_load.

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 import variable_load_5
-var = dir(variable_load_5)
 if __name__ == "__main__":
-    for i in range(len(var)):
-        if var[i] == 'a':
-            print(variable_load_5.a)
+    print(variable_load_5.a)
