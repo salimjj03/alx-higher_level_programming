2f889895ee6804e3466e060f6fe9b57679d079bf
salim aliyu musawa <salemaliyu1992@gmail.com>
2023-10-07 06:22:10 -0700

    print matrix

--- 0x03-python-data_structures/6-main.py
+++ 0x03-python-data_structures/6-main.py
@@ -2,9 +2,9 @@
 print_matrix_integer = __import__('6-print_matrix_integer').print_matrix_integer
 
 matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
+    [7, 7, 6],
+    [4, 8, 1],
+    [6, 4, 4]
 ]
 
 print_matrix_integer(matrix)
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix:
-        for i in matrix:
-            for j in i:
-                print("{:d}".format(j), end=" ")
-            print()
+    for i in matrix:
+        len_i = len(i) - 1
+        for j in range(len(i)):
+            if j != len_i:
+                print("{:d}".format(i[j]), end=" ")
+            else:
+                print("{:d}".format(i[j]), end="")
+        print()
