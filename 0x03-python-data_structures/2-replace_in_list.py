7fdccdf66148d4316ba6d09fa772bd479c8773cd
salim aliyu musawa <salemaliyu1992@gmail.com>
2023-10-07 03:25:20 -0700

    replace in list

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,5 +3,4 @@ def replace_in_list(my_list, idx, element):
     if idx < len(my_list) and idx >= 0:
         my_list[idx] = element
         return my_list
-    else:
-        return my_lsit
+    return my_list
