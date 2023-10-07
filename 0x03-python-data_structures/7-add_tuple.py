677ae53706257c3a2f48a97ca539a7a447f27ddb
salim aliyu musawa <salemaliyu1992@gmail.com>
2023-10-07 09:19:10 -0700

    add tulpe

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -8,4 +8,6 @@ def add_tuple(tuple_a=(), tuple_b=()):
         new_tuple = (tuple_a[0] + tuple_b[0], 0 + tuple_b[1])
     elif len(tuple_a) >= 2 and len(tuple_b) == 1:
         new_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + 0)
+
+
 return new_tuple
