cc047b2f6a57fb858f447ee81aa12c3c35f9cdd4
salim aliyu musawa <salemaliyu1992@gmail.com>
2023-10-07 09:32:27 -0700

    add tulpe

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -3,3 +3,4 @@ def add_tuple(tuple_a=(), tuple_b=()):
     new_ta = tuple_a + (0, 0)
     new_tb = tuple_b + (0, 0)
     new_tuple = (new_ta[0] + new_tb[0], new_ta[1] + new_tb[1])
+    return new_tuple
