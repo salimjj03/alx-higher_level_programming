26ac0ba521c46a0175860d4adbad09570c7714ff
salim aliyu musawa <salemaliyu1992@gmail.com>
2023-10-07 11:12:55 -0700

    divisible by 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,6 +3,8 @@ def divisible_by_2(my_list=[]):
     if len(my_list) > 0:
         new_list = []
         for i in my_list:
+            if i < 0:
+                i = i * -1
             if i % 2 == 0:
                 new_list.append("True")
             else:
