a91b98e3bd2203c4544a3b677aa679858a8b9725
salim aliyu musawa <salemaliyu1992@gmail.com>
2023-10-08 02:31:51 -0700

    is palindeome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * is_palindrome - ...
+ * @head: ..
+ * Return: ...
+ */
+
+int is_palindrome(listint_t **head)
+{
+	listint_t *tmp_e;
+	int v[100], i = 0;
+
+	if (*head == NULL)
+		return (1);
+	tmp_e = *head;
+	while (tmp_e != NULL)
+	{
+		v[i] = tmp_e->n;
+		i++;
+		tmp_e = tmp_e->next;
+	}
+	tmp_e = *head;
+	i = i - 1;
+	while (tmp_e->next != NULL)
+	{
+		if (tmp_e->n != v[i])
+			return (0);
+		tmp_e = tmp_e->next;
+		i--;
+	}
+	return (1);
+}
