5aa539aa1b4ce611f5a2d74978eac1e20181222d
salim aliyu musawa <salemaliyu1992@gmail.com>
2023-10-08 03:01:02 -0700

    is palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,5 +1,6 @@
 #include "lists.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * is_palindrome - ...
@@ -10,11 +11,19 @@
 int is_palindrome(listint_t **head)
 {
 	listint_t *tmp_e;
-	int v[100], i = 0;
+	int *v, i = 0;
 
 	if (*head == NULL || (*head)->next == NULL)
 		return (1);
 	tmp_e = *head;
+	while (tmp_e != NULL)
+	{
+		i++;
+		tmp_e = tmp_e->next;
+	}
+	v = malloc(sizeof(int) * i);
+	tmp_e = *head;
+	i = 0;
 	while (tmp_e != NULL)
 	{
 		v[i] = tmp_e->n;
@@ -26,9 +35,13 @@ int is_palindrome(listint_t **head)
 	while (tmp_e->next != NULL)
 	{
 		if (tmp_e->n != v[i])
+		{
+			free(v);
 			return (0);
+		}
 		tmp_e = tmp_e->next;
 		i--;
 	}
+	free(v);
 	return (1);
 }
