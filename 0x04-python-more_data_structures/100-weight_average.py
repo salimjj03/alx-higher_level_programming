5612d047b3a5e4627c9ed174fad65a69e2d8e6f4
salim aliyu musawa <salemaliyu1992@gmail.com>
2023-10-11 18:02:35 -0700

    weight average

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -4,8 +4,8 @@ def weight_average(my_list=[]):
         mul = 0
         sum = 0
         for i in my_list:
-            mul = mul i[0] * i[1]
-            sum += i[1]
+            mul = mul + i[0] * i[1]
+            sum = sum + i[1]
         return (mul / sum)
     else:
         return (0)
