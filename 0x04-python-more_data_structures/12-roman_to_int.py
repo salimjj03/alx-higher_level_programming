3083312bda77940cbf06929bc0e5dbdc8bbb1257
salim aliyu musawa <salemaliyu1992@gmail.com>
2023-10-15 06:53:24 -0700

    roman_to_int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     n = 0
-    if roman_string == '':
+    if roman_string == '' or !roman_string:
         return 0
     digit = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     for i in range(len(roman_string)):
