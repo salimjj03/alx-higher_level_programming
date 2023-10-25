d762f1c4ab474b7eee1771e7c2b90abb4b7b11f6
sali aliyu musawa <salemaliyu1992@gmail.com>
2023-10-25 21:26:16 +0000

    4 square

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+
+"""This modul find Sequare 0f number."""
+
+
+class Square:
+    """Find the squareroot of a number."""
+
+    def __init__(self, size=0):
+        """This method initialize values"""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """This Method find area"""
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """The summary line for a class docstring should fit on one line."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """The summary line for a class docstring should fit on one line."""
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
