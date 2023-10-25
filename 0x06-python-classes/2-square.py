6d4e96a4d944566d173a45096313997adf26e08e
sali aliyu musawa <salemaliyu1992@gmail.com>
2023-10-25 03:39:01 +0000

    2 square

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,40 +1,27 @@
 #!/usr/bin/python3
+
+
+""" This modul find Sequare 0f number """
+
+
 class Square:
-	def __init__(self, size=0):
-		if type(size) != int:
-			raise TypeError("size must be an integer")
-		elif size < 0:
-			raise ValueError("size must be >= 0")
-		self.__size = size
-		
-my_square_1 = Square(3)
-print(type(my_square_1))
-print(my_square_1.__dict__)
+    
+    """
+    A squareroot class
 
-my_square_2 = Square()
-print(type(my_square_2))
-print(my_square_2.__dict__)
+    This class have he follownig method:
+    - __init---: initialaze the values
+    """
 
-try:
-    print(my_square_1.size)
-except Exception as e:
-    print(e)
+    def __init__(self, size=0):
+	"""
+	This method initialize values
 
-try:
-    print(my_square_1.__size)
-except Exception as e:
-    print(e)
-
-try:
-    my_square_3 = Square("3")
-    print(type(my_square_3))
-    print(my_square_3.__dict__)
-except Exception as e:
-    print(e)
-
-try:
-    my_square_4 = Square(-89)
-    print(type(my_square_4))
-    print(my_square_4.__dict__)
-except Exception as e:
-    print(e)
+	:param1: the size
+	"""
+	if type(size) != int:
+	    raise TypeError("size must be an integer")
+	elif size < 0:
+   	    raise ValueError("size must be >= 0")
+	else:
+	    self.__size = size
