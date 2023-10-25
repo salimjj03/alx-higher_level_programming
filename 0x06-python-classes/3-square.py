366cd7452b1e42c1407eba7f3bf09f1c67796edf
sali aliyu musawa <salemaliyu1992@gmail.com>
2023-10-25 03:59:24 +0000

    3 square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -17,11 +17,19 @@ class Square:
         """
         This method initialize values
 
-        :param1: the size
-        """
+	:param1: the size
+	"""
         if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
+
+    def area(self):
+        """
+        This Method find area
+	
+	:return: return the area
+        """
+        return self.__size ** 2
