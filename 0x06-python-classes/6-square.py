db2bf178952d6056112d644f3fc121b7b95f2584
sali aliyu musawa <salemaliyu1992@gmail.com>
2023-10-25 23:13:15 +0000

    6 square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -54,8 +54,9 @@ class Square:
     @position.setter
     def position(self, value):
         """The summary line for a class docstring should fit on one line."""
-        if (type(value) != tuple or len(value) != 2 or
-                type(value[0]) != int or type(value[1]) != int or
+         if (type(value) is not tuple or len(value) != 2 or
+                type(value[0]) is not int or
+                type(value[1]) is not int or
                 value[0] < 0 or value[1] < 0):
                 raise TypeError("position must be a tuple of 2 positive integers")
         else:
