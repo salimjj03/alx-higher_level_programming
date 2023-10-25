780ca3f14e3661e4075fc598b50f2095029dcf4b
sali aliyu musawa <salemaliyu1992@gmail.com>
2023-10-25 22:44:39 +0000

    6 square

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+
+
+"""This modul find Sequare 0f number."""
+
+
+class Square:
+    """Find the squareroot of a number."""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """This method initialize values"""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+        self.__position = position
+
+    def area(self):
+        """This Method find area"""
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """The summary line for a class docstring should fit on one line."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """The summary line for a class docstring should fit on one line."""
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def my_print(self):
+        """The summary line for a class docstring should fit on one line."""
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                print("{}".format(self.__position[0] * " "), end="")
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
+    @property
+    def position(self):
+        """The summary line for a class docstring should fit on one line."""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """The summary line for a class docstring should fit on one line."""
+        if type(position[0]) or type(position[1]) != int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
