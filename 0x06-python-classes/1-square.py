3124a37cfae869f43b34514d781dedaec9d99485
sali aliyu musawa <salemaliyu1992@gmail.com>
2023-10-25 03:01:29 +0000

    0 square 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" .......... """
+class Square:
+	
+    """
+    A class that define sqaure
+    Atribut:
+	__size (int): value of the sqare
+    """
+    def __init__(self, size):
+
+	"""
+	A special fun
+
+	Args:
+	   siz (int): .....
+	 	"""
+	self.__size = size
