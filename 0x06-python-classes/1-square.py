ba2612cf0183480344224230a84b75e0902db1f6
sali aliyu musawa <salemaliyu1992@gmail.com>
2023-10-25 09:46:14 +0000

    square

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -13,11 +13,6 @@ with a section header and a colon followed by a block of indented text.
 class Square:
     """The summary line for a class docstring should fit on one line.
 
-    If the class has public attributes, they may be documented here
-    in an ``Attributes`` section and follow the same formatting as a
-    function's ``Args`` section. Alternatively, attributes may be documented
-    inline with the attribute's declaration (see __init__ method below).
-
     Properties created with the ``@property`` decorator should be documented
     in the property's getter method.
 
