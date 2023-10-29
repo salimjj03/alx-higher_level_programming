ae799ee56b03c43af148c13851d00d5431b47d9a
salim aliyu musawa <salemaliyu1992@gmail.co>
2023-10-29 09:37:14 -0700

    max int

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+class test_max_integer(unittest.TestCase):
+    """ This test the flowwing methonds.
+
+    """
+
+    def test_values(self):
+        """ This test the values.
+
+        """
+
+        self.assertAlmostEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertAlmostEqual(max_integer([1, 3, 4, 2]), 4)
+        self.assertAlmostEqual(max_integer([]), None)
+
+
+if __name__ == "__main__":
+    unittest.main()
