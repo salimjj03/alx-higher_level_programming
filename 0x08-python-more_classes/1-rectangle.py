02cb423c0375926acaeaa2e92a3a508663669ece
salim aliyu musawa <salemaliyu1992@gmail.co>
2023-10-30 12:53:13 -0700

    1 rectangle

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+
+
+"""
+
+This is empty class
+
+"""
+
+
+class Rectangle:
+    """ defines an empty class rectangle """
+
+    def __init__(self, width=0, height=0):
+        """ Method that initialize the values. """
+
+        self.__height = height
+        self.__width = width
+
+    @property
+    def width(self):
+        """ This method get the width. """
+
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ This method set the width. """
+
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """ This method get the heiget. """
+
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ This method set the height. """
+
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
