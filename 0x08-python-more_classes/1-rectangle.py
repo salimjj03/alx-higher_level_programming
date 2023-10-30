ab8c7c58f74248724233e15606bba4ec6a857508
salim aliyu musawa <salemaliyu1992@gmail.co>
2023-10-30 13:04:01 -0700

    1 rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -15,15 +15,15 @@ class Rectangle:
         """ Method that initialize the values. """
 
         if type(width) != int:
-            raise TypeError("height must be an integer")
+            raise TypeError("width must be an integer")
         if width < 0:
-            raise ValueError("height must be >= 0")
+            raise ValueError("width must be >= 0")
         if type(height) != int:
             raise TypeError("height must be an integer")
         if height < 0:
             raise ValueError("height must be >= 0")
-        self.__height = height
         self.__width = width
+        self.__height = height
 
     @property
     def width(self):
@@ -36,9 +36,9 @@ class Rectangle:
         """ This method set the width. """
 
         if type(value) != int:
-            raise TypeError("height must be an integer")
+            raise TypeError("width must be an integer")
         if value < 0:
-            raise ValueError("height must be >= 0")
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
