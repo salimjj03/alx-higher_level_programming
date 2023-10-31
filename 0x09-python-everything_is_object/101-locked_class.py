f63bc5ea615f2348b4cc2e083728054695239210
salim aliyu musawa <salemaliyu1992@gmail.co>
2023-10-31 14:07:40 -0700

    lock class

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+""" This module locked class """
+
+
+class LockedClass:
+    """ The class that allow only attr called first_name
+
+    """
+
+    __slots__ = ["first_name"]
