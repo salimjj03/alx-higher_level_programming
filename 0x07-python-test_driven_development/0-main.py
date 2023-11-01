c3761ec8fb4d6fe05093262504684510166073ca
salim aliyu musawa <salemaliyu1992@gmail.co>
2023-11-01 03:32:46 -0700

    add int

--- 0x07-python-test_driven_development/0-main.py
+++ 0x07-python-test_driven_development/0-main.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 add_integer = __import__('0-add_integer').add_integer
+import math
 
-print(add_integer(1, 2))
+x = float("nan")
+
+print(add_integer(1, x))
 print(add_integer(100, -2))
 print(add_integer(2))
 print(add_integer(100.3, -2))
