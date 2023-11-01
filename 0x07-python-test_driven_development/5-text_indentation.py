de7c1742db4fe7f7867d7f4052cc6056947e11d5
salim aliyu musawa <salemaliyu1992@gmail.co>
2023-11-01 14:08:54 -0700

    5-max_integer_test

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -17,7 +17,9 @@ def text_indentation(text):
     if type(text) != str:
         raise TypeError("text must be a string")
     for i in range(len(text)):
-        if text[i - 1] not in ".?:" and text[i] != " ":
+        if text[i] not in ".?:":
+            if text[i - 1] in ".?:" and text[i] != " ":
+                pass
             print(text[i], end="")
-            if text[i] in ".?:":
-                print("\n")
+        if text[i] in ".?:":
+            print("\n")
