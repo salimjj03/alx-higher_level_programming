bb374863778e90bbdb03615e5a89f731b29d7123
salim aliyu musawa <salemaliyu1992@gmail.co>
2023-11-01 15:46:15 -0700

    101

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+"""
+
+
+    Module to find the max integer in a list
+
+
+"""
+
+
+import numpy as np
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """ The functio that does the work """
+    return np.dot(m_a, m_b)
