81b319460e07729058cf075af54ebbeac98a3a07
salim aliyu musawa <salemaliyu1992@gmail.co>
2023-11-01 03:04:54 -0700

    add int

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -12,12 +12,8 @@ def add_integer(a, b=98):
     """ The function find the sum of two integers.
 
     """
-    if type(a) != int and type(a) != float:
+    if type(a) is not int and type(a) != float:
         raise TypeError("a must be an integer")
-    if type(b) != int and type(b) != float:
+    if type(b) is not int and type(b) != float:
         raise TypeError("b must be an integer")
-    if type(a) == float:
-        a = int(a)
-    if type(b) == float:
-        b = int(b)
-    return a + b
+    return int(a + b)
