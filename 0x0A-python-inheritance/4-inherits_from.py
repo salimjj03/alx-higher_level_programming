aef8c57bc31abc97e8e81e7c2ad208225c1d42df
salim aliyu musawa <salemaliyu1992@gmail.co>
2023-11-06 04:33:27 -0800

    inherite from

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+
+This module chech instance of the specified class.
+
+"""
+
+
+def inherits_from(obj, a_class):
+    """ This class does the work.
+
+    """
+
+    return issubclass(obj, a_class)
