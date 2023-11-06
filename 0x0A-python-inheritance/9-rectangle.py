49ec2eda22419e2fe8ce9a15bc3c90908743b5c4
salim aliyu musawa <salemaliyu1992@gmail.co>
2023-11-06 07:55:02 -0800

    9 rectangle

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+"""
+
+This module chech instance of the specified class.
+
+"""
+
+
+class BaseGeometry:
+    """ This class does the work.
+
+    """
+
+    def area(self):
+        """ Raise exception.
+
+        """
+
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ This validate int.
+
+        """
+
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+        self.name = name
+        self.value = value
+
+
+class Rectangle(BaseGeometry):
+    """ This class is sub class of super class.
+
+    """
+
+    def __init__(self, width, height):
+        """ This method is a constructor.
+
+        """
+        super().integer_validator("height", height)
+        super().integer_validator("width", width)
+        self.__height = height
+        self.__width = width
+
+    def area(self):
+        """ This method return area.
+
+        """
+
+        return self.__height * self.__width
+
+    def __str__(self):
+        """ Print classname and area.
+
+        """
+
+        return "[{}] {}/{}" \
+            .format(__class__.__name__, self.__width, self.__height)
