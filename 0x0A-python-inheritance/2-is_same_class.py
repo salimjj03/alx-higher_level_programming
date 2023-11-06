68c9a468f56c8dff2ca0bed3770650f087c19b29
salim aliyu musawa <salemaliyu1992@gmail.co>
2023-11-06 03:57:52 -0800

    is same class

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -11,7 +11,7 @@ def is_same_class(obj, a_class):
 
     """
 
-    if isinstance(obj, a_class) == True:
+    if isinstance(type(obj), a_class) == True:
         return True
     else:
         return False
