86dc8199bf55b39a6e3abb89480f1e0fcf2b1cbf
salim aliyu musawa <salemaliyu1992@gmail.co>
2023-11-06 04:46:10 -0800

    Raise exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+
+This module chech instance of the specified class.
+
+"""
+
+
+class BaseGeometry:
+    """ This class does the work.
+
+    """
+
+    def area(self):
+        """ Raise exception.
+
+        """
+
+        raise Exception("area() is not implemented")
