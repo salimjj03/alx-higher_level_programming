a7cb6b7cb8f9348a71d6dc0913d4bb802e8876f6
salim aliyu musawa <salemaliyu1992@gmail.co>
2023-11-06 04:30:37 -0800

    kind of class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+
+This module chech instance of the specified class.
+
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """ This class does the work.
+
+    """
+
+    return isinstance(obj, a_class)
