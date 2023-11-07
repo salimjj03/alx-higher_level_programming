e4fbbfa474172f886783e5370cbf1c69bf7eb78a
salim aliyu musawa <salemaliyu1992@gmail.co>
2023-11-07 14:18:18 -0800

    stdunts

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+
+This Module reads a text file.
+
+"""
+
+
+class Student():
+    """ This function dose the work.
+
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """ The custructor method
+
+        """
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Htis is another method.
+
+        """
+
+        ls = {}
+        if type(attrs) != []:
+            return self.__dict__
+
+        else:
+            for i in attrs:
+                if type(i) != str:
+                    return self.__dict__
+            for j in self.__dict__:
+                if j in attrs:
+                    dic[j] = self.__dict__[j]
+        return dic
