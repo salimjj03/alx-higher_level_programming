b9d1534cab9d2b367b627c5b63f299afd7408a0a
salim aliyu musawa <salemaliyu1992@gmail.co>
2023-11-07 13:30:53 -0800

    class to json

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+
+This Module reads a text file.
+
+"""
+
+
+import json
+
+
+def class_to_json(obj):
+    """ This function dose the work.
+
+    """
+
+    return obj.__dict__
