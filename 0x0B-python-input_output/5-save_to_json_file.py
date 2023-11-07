a3c687c0090754b8a21c0f8fa4c370bcd09d4207
salim aliyu musawa <salemaliyu1992@gmail.co>
2023-11-07 06:51:51 -0800

    from json

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+
+This Module reads a text file.
+
+"""
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """ This function dose the work.
+
+    """
+
+    myjson = json.dump(my_obj, filename)
+    return myjson
