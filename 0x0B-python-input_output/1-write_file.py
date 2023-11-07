8c1207a4706444c2b97f9e27c7e77384eec18089
salim aliyu musawa <salemaliyu1992@gmail.co>
2023-11-07 06:18:22 -0800

    write file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+
+This Module reads a text file.
+
+"""
+
+
+def write_file(filename="", text=""):
+    """ This function dose the work.
+
+    """
+
+    with open(filename, "w", encoding="UTF8") as myfile:
+       size =  myfile.write(text)
+    return size
