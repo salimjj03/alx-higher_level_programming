f1ec922c49519c1ab2d9e5c3990b2254d5ec3344
salim aliyu musawa <salemaliyu1992@gmail.co>
2023-12-12 12:18:42 -0800

    add recod

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,3 @@
+-- This script lists all databases of your MySQL server.
+-- Display all databases.
+SHOW CREATE TABLE first_table;
---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,3 @@
+-- This script lists all databases of your MySQL server.
+-- Display all databases.
+INSERT INTO first_table (id, name) VALUES (89, "Best School");
