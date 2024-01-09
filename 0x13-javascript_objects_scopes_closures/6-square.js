7846b252b110cfceb7ae5d9c0db9f89583d0e32a
salim aliyu musawa <salemaliyu1992@gmail.co>
2024-01-09 14:20:21 -0800

    square

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+module.exports = class Square extends require('./4-rectangle.js') {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c) {
+      for (let i = 0; i < this.width; i++) console.log(c.repeat(this.width));
+    } else {
+      this.print();
+    }
+  }
+};
