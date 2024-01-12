63318dd53a6f3710af63c55de752143456e50c68
salim aliyu musawa <salemaliyu1992@gmail.co>
2024-01-12 11:51:25 -0800

    flters

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" This Module quary a database """
+
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3]
+            )
+
+    cur = db.cursor()
+    st = argv[4]
+    cur.execute("SELECT name FROM cities WHERE state_id = ( \
+            SELECT id FROM states WHERE name = %s) \
+            ORDER BY id", (st,))
+
+    result = cur.fetchall()
+    for row in result:
+        print("{}, ".format(row[0])
+              if row != result[-1]
+              else "{}{}".format(row[0], "\n"), end="")
+
+    cur.close()
+    db.close()
