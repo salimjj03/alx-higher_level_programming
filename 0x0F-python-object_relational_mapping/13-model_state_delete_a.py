28bc0b029fdd4fabc84114f76af8bbd6f8bac117
salim aliyu musawa <salemaliyu1992@gmail.co>
2024-01-13 05:20:01 -0800

    state delete

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" This Module prints the first State object
+from the database hbtn_0e_6_usa. """
+
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = "mysql://{}:{}@localhost/{}".format(argv[1], argv[2], argv[3])
+    engine = create_engine(url, pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    delete_r = session.query(State).filter(State.name.contains('a')).all()
+    if delete_r:
+        for r in delete_r:
+            session.delete(r)
+        session.commit()
