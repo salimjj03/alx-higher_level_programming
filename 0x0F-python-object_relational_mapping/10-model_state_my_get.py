3a49bb4685c4f708b9bc6e24ff8b6ad20c6e8b8f
salim aliyu musawa <salemaliyu1992@gmail.co>
2024-01-13 04:52:50 -0800

    my get

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" This Module prints the first State object
+from the database hbtn_0e_6_usa. """
+
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = "mysql://{}:{}@localhost/{}".format(argv[1], argv[2], argv[3])
+    engine = create_engine(url, pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    result = session.query(State).filter(State.name == argv[4]).all()
+    if result:
+        print(result[0].id)
+    else:
+        print("Not found")
