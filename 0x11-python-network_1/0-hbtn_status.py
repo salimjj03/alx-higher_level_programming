9097d69c2b397ba3bf719fcf459b6dc3f130ea54
salim aliyu musawa <salemaliyu1992@gmail.co>
2024-01-26 12:45:06 -0800

    get request

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" this model fetches https://alx-intranet.hbtn.io/status. """
+
+from urllib.request import urlopen
+
+
+if __name__ == "__main__":
+    with urlopen("https://alx-intranet.hbtn.io/status") as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" THis model  takes in a URL, sends a request
+to the URL and displays the:wq
+value of the X-Request-Id variable f
+ound in the header of the response. """
+
+imd
