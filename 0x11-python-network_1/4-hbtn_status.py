d7301e9a9eb4f8cb05f27de257f5caefb3454c35
salimjj03 <salimafinisher@gmail.com>
2024-01-26 16:16:08 -0800

    stats

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -9,6 +9,6 @@ import requests
 
 if __name__ == "__main__":
     with requests.get("https://alx-intranet.hbtn.io/status") as r:
-        print("Body response:$")
+        print("Body response:")
         print("\t- type: {}".format(type(r.text)))
         print("\t- content: {}".format(r.text))
